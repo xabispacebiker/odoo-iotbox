f2c12de2cda442aae08185da69384d2bd335ff95
Odoo Masters <info@odoomasters.com>
2023-07-12 17:03:47 +0200

    [UPD] general update

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -1145,16 +1145,12 @@ class WorkerCron(Worker):
 server = None
 
 def load_server_wide_modules():
-    server_wide_modules = {'base', 'web'} | set(odoo.conf.server_wide_modules)
+    server_wide_modules = {'base'} | set(odoo.conf.server_wide_modules)
     for m in server_wide_modules:
         try:
             odoo.modules.module.load_openerp_module(m)
         except Exception:
             msg = ''
-            if m == 'web':
-                msg = """
-The `web` module is provided by the addons found in the `openerp-web` project.
-Maybe you forgot to add those addons in your addons_path configuration."""
             _logger.exception('Failed to load server-wide module `%s`.%s', m, msg)
 
 def _reexec(updated_modules=None):
